85234759ab1322eef7083dc9e2f4287ec71adafa
Corbin <corbin.jensen@me.com>
2023-05-02 12:37:57 -0400

    remove google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
-import NavContext from '../context/NavContext'
 import {
   Favicons,
   Hero,
@@ -12,26 +10,12 @@ import {
 } from '../components'
 
 export default function Home() {
-  const setupAnalytics = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments)}
-    gtag('js', new Date());
-
-    gtag('config', 'G-4YCFJVTCZR');
-  }
-
-  useEffect(() => {
-    setupAnalytics();
-  }, [])
-
   return (
     <div>
       <Head>
         <title>Corbin Jensen's Portfolio</title>
         <meta name="description" content="Corbin Jensen is a Frontend Developer and this is his portfolio website. He's a frontend developer in the Charlotte, NC area." />
         <Favicons />
-        // Global site tag (gtag.js) - Google Analytics
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4YCFJVTCZR"></script>
       </Head>
 
       <header>
