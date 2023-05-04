6f2c07e0ad67dbcf42b723c9d155a486d6c0e683
Corbin <corbin.jensen@me.com>
2023-05-04 14:13:50 -0400

    update project section

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -10,7 +10,7 @@ export function ProjectSection() {
 
       <div className="project-wrap">
         <ul className="projects container">
-        <li className="project">
+          <li className="project">
             <div
               className="project__image"
               style={{ backgroundImage: `url('Alto.svg'); background-color: #0c6b5b` }}></div>
@@ -26,13 +26,26 @@ export function ProjectSection() {
           <li className="project">
             <div
               className="project__image"
-              style={{ backgroundImage: `url('steel_logo.png'); background-color: #bf1e2e` }}></div>
+              style={{ backgroundImage: `url('hl_logo.jpg'); background-color: #fff` }}></div>
             <div className="project__content">
-              <h5 className="font-gradient section-subtitle project__category">Freelance</h5>
-              <h3 className="project__title">Steel Virtue Group</h3>
-              <p className="project__desc">This was a freelance project for a guns and ammo dealer. I used their 3rd party inventory management API to populate the website with their products. This was built with React/Next.js.</p>
+              <h5 className="font-gradient project__category">Career</h5>
+              <h3 className="project__title">Healthline</h3>
+              <p className="project__desc">Led frontend team in planning, development and deployment of a new health education product. This resulted in acquiring 12,000+ new users in the first 2 months. This was built with React, Tailwind CSS, Next.js.</p>
               <div className="project__buttons">
-                <a className="project__link" href="https://github.com/corbinmj16/steel-virtue-website" target="_blank">Github</a>
+                <a className="project__link" href="https://www.healthline.com" target="_blank">Website</a>
+              </div>
+            </div>
+          </li>
+          <li className="project">
+            <div
+              className="project__image"
+              style={{ backgroundImage: `url('bezzy_logo.png'); background-color: #fff` }}></div>
+            <div className="project__content">
+              <h5 className="font-gradient project__category">Career</h5>
+              <h3 className="project__title">Bezzy (Healthline)</h3>
+              <p className="project__desc">Individual contributor on this social network for chronic illnesses. This was both a web app and native iOS/Android app. The web app was built with React/Next.js and the native apps were built with React Native.</p>
+              <div className="project__buttons">
+                <a className="project__link" href="https://www.healthline.com" target="_blank">Website</a>
               </div>
             </div>
           </li>
@@ -52,13 +65,27 @@ export function ProjectSection() {
           <li className="project">
             <div
               className="project__image"
-              style={{ backgroundImage: `url('hl_logo.jpg'); background-color: #fff` }}></div>
+              style={{ backgroundImage: `url('scc_logo.png'); background-color: #373737` }}></div>
             <div className="project__content">
-              <h5 className="font-gradient project__category">Career</h5>
-              <h3 className="project__title">Healthline</h3>
-              <p className="project__desc">Led frontend team in planning, development and deployment of a new health education product. This resulted in acquiring 12,000+ new users in the first 2 months. This was built with React, Tailwind CSS, Next.js.</p>
+              <h5 className="font-gradient section-subtitle project__category">Freelance</h5>
+              <h3 className="project__title">Step Change Consulting</h3>
+              <p className="project__desc">This is a POC for an order-request system to improve efficiency in a factory my father-in-law runs. This is built with React/Next.js, Supabase (database) and Tailwind.</p>
               <div className="project__buttons">
-                <a className="project__link" href="https://www.healthline.com" target="_blank">Website</a>
+                <a className="project__link" href="https://github.com/corbinmj16/step-change-requests" target="_blank">Github</a>
+                <a className="project__link" href="https://step-change-requests.vercel.app/" target="_blank">Website</a>
+              </div>
+            </div>
+          </li>
+          <li className="project">
+            <div
+              className="project__image"
+              style={{ backgroundImage: `url('steel_logo.png'); background-color: #bf1e2e` }}></div>
+            <div className="project__content">
+              <h5 className="font-gradient section-subtitle project__category">Freelance</h5>
+              <h3 className="project__title">Steel Virtue Group</h3>
+              <p className="project__desc">This was a freelance project for a guns and ammo dealer. I used their 3rd party inventory management API to populate the website with their products. This was built with React/Next.js.</p>
+              <div className="project__buttons">
+                <a className="project__link" href="https://github.com/corbinmj16/steel-virtue-website" target="_blank">Github</a>
               </div>
             </div>
           </li>
